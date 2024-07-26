a3b7367e40eec1f4432d329d861ed2f27741a78d
wewantwebsites <cas.guzdziol@wewantweb.site>
2024-07-25 23:46:16 -0400

    added a delegate, the list renders now and isn't freaking out

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -18,17 +19,19 @@ import (
 )
 
 type errMsg error
-type listItem struct {
-	name   string
-	number int
+
+type Item struct {
+	title, desc string
 }
 
-type model struct {
-	spinner  spinner.Model
-	quitting bool
-	err      error
-	pokemon  structs.Pokemon
-	bag      map[int]string
+func (i Item) Title() string {
+	return i.title
+}
+func (i Item) Description() string {
+	return i.desc
+}
+func (i Item) FilterValue() string {
+	return i.title
 }
 
 var quitKeys = key.NewBinding(
@@ -47,17 +50,45 @@ var listKeys = key.NewBinding(
 
 var (
 	pokeCard = lipgloss.NewStyle().
-		Border(lipgloss.NormalBorder()).
-		Padding(2, 4).
-		Width(40)
+			Border(lipgloss.NormalBorder()).
+			Padding(2, 4).
+			Width(40)
+
+	appStyle   = lipgloss.NewStyle().Padding(1, 2)
+	titleStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#25A065")).
+			Padding(0, 1)
+
+	statusMessageStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#044f9f"}).
+				Render
+	// make styles:
+	// [ ] pokemon with name, summary, stats, 4 random moves, number, typing
 )
 
 func initialModel() model {
-	m := map[int]string{}
+	i := []list.Item{}
+	var delegateKeys = newDelegateKeyMap()
+	delegate := newItemDelegate(delegateKeys)
+	l := list.New(i, delegate, 0, 0)
+	l.Title = "PokeDex"
+	b := make(map[int]structs.Pokemon)
 	s := spinner.New()
 	s.Spinner = spinner.Dot
 	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model{spinner: s, bag: m}
+
+	return model{spinner: s, bag: b, list: l, delegateKeys: delegateKeys}
+}
+
+type model struct {
+	spinner      spinner.Model
+	quitting     bool
+	err          error
+	pokemon      structs.Pokemon
+	list         list.Model
+	bag          map[int]structs.Pokemon
+	delegateKeys *delegateKeyMap
 }
 
 func (m model) Init() tea.Cmd {
@@ -65,72 +96,87 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
 	switch msg := msg.(type) {
-
+	case tea.WindowSizeMsg:
+		h, v := appStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v-15)
 	case tea.KeyMsg:
-		if key.Matches(msg, quitKeys) {
+		if m.list.FilterState() == list.Filtering {
+			break
+		}
+
+		switch {
+		case key.Matches(msg, quitKeys):
 			m.quitting = true
 			return m, tea.Quit
-		}
-		if key.Matches(msg, fetchKeys) {
+		case key.Matches(msg, fetchKeys):
 			i := rand.Intn(151) + 1
-			for m.bag[i] != "" {
+			for _, ok := m.bag[i]; ok; _, ok = m.bag[i] {
 				i = rand.Intn(151) + 1
 			}
 			p, err := pokeapi.Pokemon(strconv.Itoa(i))
 			if err != nil {
 				log.Fatal("there was an error retrieving a pokemon", err)
 			}
-
-			m.bag[p.Order] = p.Name
+			item := Item{title: p.Name, desc: fmt.Sprint(p.Order)}
+			insertCmd := m.list.InsertItem(0, item)
+			m.bag[p.Order] = p
+			pokeDexSize := len(m.bag)
 			m.pokemon = p
+			title := fmt.Sprintf("%s\t%d pokemon caught!", m.list.Title, pokeDexSize)
+			statusCmd := m.list.NewStatusMessage(statusMessageStyle(title))
+			cmds = append(cmds, insertCmd, statusCmd)
 		}
-		return m, nil
+		// case key.Matches(msg, listKeys):
+
+		return m, tea.Batch(cmds...)
+
 	case errMsg:
 		m.err = msg
 		return m, nil
 
 	default:
-		var cmd tea.Cmd
-
-		m.spinner, cmd = m.spinner.Update(msg)
+		// we are waiting people
+		newSpinner, cmd := m.spinner.Update(msg)
+		m.spinner = newSpinner
 		return m, cmd
 	}
-}
 
-func (m *model) printBag() string {
-	var sb strings.Builder
+	// update the list ery time
+	newListModel, cmd := m.list.Update(msg)
+	m.list = newListModel
 
-	for k, v := range m.bag {
-		fmt.Fprintf(&sb, "poke: %s\t#%d\n", v, k)
-	}
-	return sb.String()
+	return m, cmd
 }
 
 func (m model) View() string {
 	if m.err != nil {
-		return m.err.Error()
+		return appStyle.Render(m.err.Error())
 	}
 
 	pokelabel := "Fetch a pokemon!"
 	var sb strings.Builder
 
-	fmt.Fprint(&sb, pokeCard.Render(pokelabel, m.pokemon.Name, m.printBag()))
+	fmt.Fprint(&sb, pokeCard.Render(pokelabel, m.pokemon.Name))
 	if m.pokemon.Name == "" {
 		fmt.Fprintf(&sb, "\n\n%s\tWaiting for a command...", m.spinner.View())
+	} else {
+		fmt.Fprintf(&sb, "\n%s", m.list.View())
 	}
 	fmt.Fprintf(&sb, "\n%s", fetchKeys.Help().Desc)
 	fmt.Fprintf(&sb, "\n%s", quitKeys.Help().Desc)
 
-	// TODO: render sprite as an image
 	if m.quitting {
 		fmt.Fprint(&sb, "\nGood Bye")
 	}
-	return sb.String()
+
+	return appStyle.Render(sb.String())
 }
 
 func main() {
 	p := tea.NewProgram(initialModel())
+
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
